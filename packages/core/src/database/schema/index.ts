405611d0c23698158dc54457b77eaab1cc172f44
TharushaDenuwan <tharushadenuwan35@gmail.com>
2026-01-20 15:42:51 +0530

    updated

--- packages/core/src/database/schema/index.ts
+++ packages/core/src/database/schema/index.ts
@@ -1,3 +1,37 @@
 export * from "./tasks.schema";
 
 export * from "./auth.schema";
+
+export * from "./media.schema";
+
+
+
+export * from "./notification.schema";
+
+export * from "./parent.schema";
+
+export * from "./teacher.schema";
+
+export * from "./feedback.schema";
+export * from "./nursery.schema";
+export * from "./payment.schema";
+
+export * from "./class.schema";
+
+export * from "./children.schema";
+
+export * from "./feedback.schema";
+
+export * from "./lessonPlans.schema";
+
+export * from "./badges.schema";
+export * from "./children.schema";
+export * from "./lessonPlans.schema";
+
+export * from "./event.schema";
+export * from "./message.schema";
+
+export * from "./conversation.schema";
+export * from "./conversationParticipant.schema";
+
+export * from "./gallery.schema";
